da24a596941565f84d6e66f9a04b85fd186ba7df
Jose Mieles <sistemas5@sico.com.co>
2025-02-03 14:52:53 -0500

    Servicio REST para registrar cliente, servicio REST conectado a SOAP

--- rest-wallet/src/main.ts
+++ rest-wallet/src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // Habilitar CORS por si se consume desde otro frontend
   await app.listen(3000);
-  console.log('REST API corriendo en http://localhost:3000');
+  console.log('REST API corriendo en http://localhost:'+process.env.PORT);
 }
 bootstrap();
